d78ef99eab2f8d42c01b154d21de6acd92373f7e
tzabolotko <tim18125@gmail.com>
2024-03-26 19:02:54 +0300

    commit 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 
+import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -10,8 +11,19 @@ public class Main {
     static BlockingQueue <String> blockingQueueB = new ArrayBlockingQueue<>(100);
     static BlockingQueue <String> blockingQueueC = new ArrayBlockingQueue<>(100);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Поехали");
+        Random random = new Random();
+
 
 
     }
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+
 }
